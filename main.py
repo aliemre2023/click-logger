1c227622d2bfe68e9ac4fd5efd1f4b84902de51e
aliemre2023 <aliemrekaya239@gmail.com>
2023-09-20 10:37:28 +0300

    time propertie

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from pynput import mouse
+import time
+
+start = time.time()
 
 def on_click(x, y, button, pressed):
     if pressed:
         print(f"Mouse clicked at ({x}, {y})")
+        current = time.time() - start
         with open("data.txt", "a") as file:
-            file.write(f"{x}, {y}\n")
+            file.write(f"{x}, {y}, {round(current, 3)}\n")
 
 while True:
     # Create a listener for mouse events
